797df6d8a7343311386f3894f57f9a14c501945d
roddwy <roddwy@gmail.com>
2019-11-15 17:12:38 -0400

    Primer commit

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,39 @@
+window._ = require('lodash');
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//require('admin-lte/plugins/jquery/jquery.min.js');
+window.$ = window.jQuery = require('admin-lte/plugins/jquery/jquery.min.js');
+require('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js');
+require('admin-lte/dist/js/adminlte.min.js');
+require('admin-lte/dist/js/demo.js');
+/*
+<script src="../../plugins/jquery/jquery.min.js"></script>
+
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../dist/js/adminlte.min.js"></script>
+<script src="../../dist/js/demo.js"></script>*/
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from 'laravel-echo';
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     encrypted: true
+// });
